4f484fea31ffa17d3991f12b50ac975d0fff2032
Metype <39036543+metype@users.noreply.github.com>
2020-07-24 05:10:15 -0500

    Add dark theme based on system preference.

--- src/main.js
+++ src/main.js
@@ -12,6 +12,15 @@ import router from './router';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
+const mq = window.matchMedia?.('(prefers-color-scheme: dark)');
+
+vuetify.framework.theme.dark = mq?.matches;
+
+// eslint-disable-next-line no-unused-expressions
+mq?.addEventListener?.('change', (e) => {
+  vuetify.framework.theme.dark = e.matches;
+});
+
 const cache = new InMemoryCache();
 const link = new HttpLink({
   uri: 'https://api.tallcraft.com/',
